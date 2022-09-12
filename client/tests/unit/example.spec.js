58b6bfaa5a100c3bb18e42f5713a3757505c728c
Constance <113229590+constancetan7@users.noreply.github.com>
2022-09-13 00:26:27 +0800

    test: refactor + add display tests

--- client/tests/unit/example.spec.js
+++ client/tests/unit/example.spec.js
@@ -1,12 +1,26 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import ShortenedUrlResult from '@/components/ShortenedUrlResult.vue'
 
 describe('ShortenedUrlResult.vue', () => {
+  let wrapper
+  let shortenedUrl = 'url'
+
+  beforeEach(() => wrapper = shallowMount(
+    ShortenedUrlResult, {
+      propsData: {shortenedUrl}
+    }
+  ))
+
   it('renders props.shortenedUrl when passed', () => {
-    const shortenedUrl = 'url'
-    const wrapper = shallowMount(ShortenedUrlResult, {
-      propsData: { shortenedUrl }
-    })
-    expect(wrapper.text()).toMatch(shortenedUrl)
+    expect(wrapper.find(`a[href="${shortenedUrl}"]`).exists()).toBeTruthy()
+    expect(wrapper.text()).toMatch('Shortened URL: ' + shortenedUrl)
   })
+
+  it('does not show content when props.shortenedUrl is empty', () => {
+    const wrapper = shallowMount(ShortenedUrlResult, {
+      propsData: {shortenedUrl: ''}
+    })
+    expect(wrapper.text()).toEqual('')
+  })
+
 })
