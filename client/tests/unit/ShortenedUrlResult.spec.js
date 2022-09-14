3185cacb2f144784239e9f42c8cd52d0545604b5
Constance <113229590+constancetan7@users.noreply.github.com>
2022-09-14 13:41:30 +0800

    tests: remove unnec arg

--- client/tests/unit/ShortenedUrlResult.spec.js
+++ client/tests/unit/ShortenedUrlResult.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
 import ShortenedUrlResult from '@/components/ShortenedUrlResult.vue'
 
 Object.assign(navigator, {
@@ -9,11 +9,11 @@ describe('ShortenedUrlResult.vue', () => {
   let wrapper
   let shortenedUrl = 'http://localhost:3000/qwe123'
 
-  const getWrapper = (shortenedUrl) => shallowMount(ShortenedUrlResult, {
+  const getWrapper = () => shallowMount(ShortenedUrlResult, {
     propsData: {shortenedUrl}
   })
 
-  beforeEach(() => wrapper = getWrapper(shortenedUrl))
+  beforeEach(() => wrapper = getWrapper())
 
   it('copies props.shortenedUrl when button clicked', () => {
     wrapper.find('button').trigger('click')
@@ -22,21 +22,21 @@ describe('ShortenedUrlResult.vue', () => {
   })
 
   describe('displays content if props.shortenedUrl passed', () => {
-    const wrapperMatchesSnapshot = (wrapper) => expect(wrapper).toMatchSnapshot()
+    const wrapperMatchesSnapshot = () => expect(wrapper).toMatchSnapshot()
 
     it('renders props.shortenedUrl when passed', () => {
       expect(wrapper.find(`a[href="${shortenedUrl}"]`).exists()).toBeTruthy()
       expect(wrapper.text()).toMatch('Shortened URL: ' + shortenedUrl)
-      wrapperMatchesSnapshot(wrapper)
+      wrapperMatchesSnapshot()
     })
 
     it('does not show content when props.shortenedUrl empty', () => {
       shortenedUrl = ''
-      wrapper = getWrapper(shortenedUrl)
+      wrapper = getWrapper()
 
       expect(wrapper.find('a').exists()).toBeFalsy()
       expect(wrapper.text()).toEqual(shortenedUrl)
-      wrapperMatchesSnapshot(wrapper)
+      wrapperMatchesSnapshot()
     })
   })
 })
